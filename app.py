a54729e3d934dab09d4f652544d788bfe1dfbcfb
yhg9sb7q9q-jpg <yhg9sb7q9q@privaterelay.appleid.com>
2026-01-10 13:25:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -103,6 +103,9 @@ def load_fixtures():
     @st.cache_data(ttl=600)
 def load_odds():
     return pd.read_csv("data/epl_odds_today.csv")
+    def is_value_bet(prob, odds, min_ev=0.05):
+    ev = (prob * odds) - 1
+    return ev >= min_ev, ev
 
 leagues = ["EPL", "LaLiga"]
 teams = list(pd.concat([load_data("EPL.csv")['HomeTeam'],
