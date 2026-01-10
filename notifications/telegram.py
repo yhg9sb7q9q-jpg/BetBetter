3b77174ffb556d62f0d8e56b0c61a0300dbfe0ba
yhg9sb7q9q-jpg <yhg9sb7q9q@privaterelay.appleid.com>
2026-01-10 03:58:27 +0000

    Create telegram.py

---
+++ notifications/telegram.py
@@ -0,0 +1,6 @@
+import requests
+
+def send_telegram(msg, token, chat_id):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {"chat_id": chat_id, "text": msg}
+    requests.post(url, data=payload)
